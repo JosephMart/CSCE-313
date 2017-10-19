b38ac8f98dfa13bfcb5aab1dec5893cd60f3cdeb
Joseph Martinsen <joseph@martinsen.com>
2017-10-19 14:16:22 -0500

    MP3 init

---
+++ mp3/cmake-build-debug/CMakeFiles/mp3.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/joseph/Documents/CSCE-313/mp3/dataserver.C" "/home/joseph/Documents/CSCE-313/mp3/cmake-build-debug/CMakeFiles/mp3.dir/dataserver.C.o"
+  "/home/joseph/Documents/CSCE-313/mp3/reqchannel.C" "/home/joseph/Documents/CSCE-313/mp3/cmake-build-debug/CMakeFiles/mp3.dir/reqchannel.C.o"
+  "/home/joseph/Documents/CSCE-313/mp3/simpleclient.C" "/home/joseph/Documents/CSCE-313/mp3/cmake-build-debug/CMakeFiles/mp3.dir/simpleclient.C.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mp3/cmake-build-debug/CMakeFiles/mp3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp3.dir/dataserver.C.o"
+  "CMakeFiles/mp3.dir/reqchannel.C.o"
+  "CMakeFiles/mp3.dir/simpleclient.C.o"
+  "mp3.pdb"
+  "mp3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mp3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
