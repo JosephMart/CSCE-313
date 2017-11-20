dfd4fe7232ffccc53bb8cde904ee86dc80064057
Joseph Martinsen <joseph@martinsen.com>
2017-11-19 22:55:06 -0600

    MP4 nearaly done

--- mp4/client.cc
+++ mp4/client.cc
@@ -5,7 +5,6 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
-#include <sys/wait.h>
 #include "reqchannel.H"
 #include "BoundedBuffer.h"
 
@@ -41,9 +40,11 @@ typedef struct stats_params {
 } STATS_PARAMS;
 
 typedef struct work_thread_params {
+    int num_request_channels;
+    int num_requests;
     BoundedBuffer<string> *buff;
     BoundedBuffer<string> **SSB_container;
-    RequestChannel *chan;
+    RequestChannel *ctl_chan;
 } WORK_THREAD_PARAMS;
 
 int HISTOGRAM[NUM_PEOPLE][NUM_BINS];
@@ -65,6 +66,7 @@ template<typename T>
 void clean_up(T *array, int _size);
 
 int main(int argc, char **argv) {
+    cout.setf(std::ios::unitbuf);
     int c = 0;
     extern char *optarg;
 
@@ -73,7 +75,7 @@ int main(int argc, char **argv) {
     // (-b) size of bounded buffer between request and worker threads
     int buffer_size = 0;
     // (-w) number of worker threads
-    int num_worker_threads = 0;
+    int num_request_channels = 0;
 
     while ((c = getopt(argc, argv, "n:b:w:")) != -1) {
         switch (c) {
@@ -84,7 +86,7 @@ int main(int argc, char **argv) {
                 buffer_size = (unsigned int) strtoul(optarg, NULL, 10);
                 break;
             case 'w':
-                num_worker_threads = (unsigned int) strtoul(optarg, NULL, 10);
+                num_request_channels = (unsigned int) strtoul(optarg, NULL, 10);
                 break;
             default:
                 break;
@@ -95,8 +97,8 @@ int main(int argc, char **argv) {
     pthread_t request_threads[NUM_PEOPLE];
     pthread_t stat_threads[NUM_PEOPLE];
 
-    // Create threads passed on passed param
-    pthread_t worker_threads[num_worker_threads];
+    // Create master worker thread
+    pthread_t worker_thread;
 
     // Start the dataserver
 //    pid_t id = fork();
@@ -114,7 +116,7 @@ int main(int argc, char **argv) {
     // ------------------------------------------------
     cout << "CLIENT STARTED:" << '\n';
     cout << "Establishing control channel... " << flush;
-    RequestChannel chan("control", RequestChannel::CLIENT_SIDE);
+    RequestChannel control_chan("control", RequestChannel::CLIENT_SIDE);
     cout << "done." << '\n';
     // ------------------------------------------------
     // Setup timer
@@ -161,23 +163,19 @@ int main(int argc, char **argv) {
     // ------------------------------------------------
     // Start w worker threads
     // ------------------------------------------------
-    RequestChannel *req_channel[num_worker_threads];
+    RequestChannel *req_channel[num_request_channels];
     string reply;
-    WORK_THREAD_PARAMS *w_t_params[num_worker_threads];
+    WORK_THREAD_PARAMS *w_t_params;
 
-    for (int i = 0; i < num_worker_threads; ++i) {
-        reply = chan.send_request("newthread");
-        cout << "Reply to request for new thread is '" << reply << "'" << '\n';
+    w_t_params = new WORK_THREAD_PARAMS{
+            num_request_channels,
+            req_per_person * 3,
+            work_buffer,    // BoundedBuffer buff;
+            stats_buffer,   // SBB_Container
+            &control_chan,    // ControlChannel;
+    };
 
-        req_channel[i] = new RequestChannel(reply, RequestChannel::CLIENT_SIDE);
-
-        w_t_params[i] = new WORK_THREAD_PARAMS{
-                work_buffer,  // BoundedBuffer buff;
-                stats_buffer, // SBB_Container
-                req_channel[i]   // RequestChannel;
-        };
-        pthread_create(&worker_threads[i], NULL, worker_thread_func, (void *) w_t_params[i]);
-    }
+    pthread_create(&worker_thread, NULL, worker_thread_func, (void *) w_t_params);
     // ------------------------------------------------
     // Wait for request threads to finish
     // ------------------------------------------------
@@ -186,14 +184,12 @@ int main(int argc, char **argv) {
     }
 
     // Kill the workers
-    for (int i = 0; i < num_worker_threads; ++i) {
-        work_buffer->Deposit("quit");
-    }
-
-    // Wait for worker threads to exit
-    for (int i = 0; i < num_worker_threads; ++i) {
-        pthread_join(worker_threads[i], NULL);
-    }
+//    for (int i = 0; i < num_request_channels; ++i) {
+//        work_buffer->Deposit("quit");
+//    }
+//    work_buffer->Deposit("quit");
+    // Wait for worker thread to exit
+    pthread_join(worker_thread, NULL);
 
     // Wait for stat threads to exit
     for (int i = 0; i < NUM_PEOPLE; ++i) {
@@ -208,16 +204,15 @@ int main(int argc, char **argv) {
     // Clean up on Aisle 4!
     //------------------------------------------------
     // Close Control
-    string reply4 = chan.send_request("quit");
+    string reply4 = control_chan.send_request("quit");
     cout << "Control Reply to request 'quit' is '" << reply4 << "'" << '\n';
+    cout << "Cleaning up Request Params\n";
     clean_up(req_params, 3);
+    cout << "Cleaning up Stats Buffer\n";
     clean_up(stats_buffer, 3);
+    cout << "Cleaning up Stats Params\n";
     clean_up(stat_params, 3);
-    clean_up(w_t_params, num_worker_threads);
-    // Loop because templating didn't work for RequestThread :(
-    for (int k = 0; k < num_worker_threads; ++k) {
-        delete req_channel[k];
-    }
+    delete w_t_params;
 
     // Wait for Data Server process to end
     // wait(NULL);
@@ -239,7 +234,7 @@ int main(int argc, char **argv) {
          << setfill(' ')
          << "Requests per Person: " << req_per_person << '\n'
          << "Buffer Size: " << buffer_size << '\n'
-         << "Number of Worker Threads: " << num_worker_threads << '\n'
+         << "Number of Worker Threads: " << num_request_channels << '\n'
          << "Requests Run Time: " << elapsed_time << " sec " << '\n';
 }
 
@@ -250,6 +245,8 @@ void *request_thread_func(void *req_args) {
         cout << "Depositing Request: " << req << '\n';
         params->buff->Deposit(req);
     }
+    params->buff->Deposit("quit");
+    cout << "Deposited (" << params->n << ") Requests\n\n";
     return 0;
 }
 
@@ -259,6 +256,7 @@ void *build_hist(void *func_params) {
     cout << "Building Histograms for " << names[params->patient_index] << '\n';
     int bin_index, value;
     for (int i = 0; i < params->n; i++) {
+//    for(;;) {
         item = params->buff->Remove();
         value = atoi(item.c_str());
         bin_index = (int) (floor(value / 10));
@@ -269,17 +267,85 @@ void *build_hist(void *func_params) {
 
 void *worker_thread_func(void *func_params) {
     WORK_THREAD_PARAMS *params = (WORK_THREAD_PARAMS *) func_params;
-    while (1) {
-        string req = params->buff->Remove();
-        cout << "Sending Request: " << req << '\n';
-        string reply = params->chan->send_request(req);
-        cout << "Response to : " << req << " --- " << reply << '\n';
-        if (req == "quit") {
-            break;
-        }
-        BoundedBuffer<string> *sbb = lookup(req, params->SSB_container);
-        sbb->Deposit(reply);
+    RequestChannel **requestChannels = new RequestChannel*[params->num_request_channels];
+
+    string *requests = new string[params->num_request_channels];
+
+    int run = 1;
+    int q = 0;
+    int max = 0;
+    int req_count = 0;
+    int quit_count = 0;
+
+    string reply;
+    RequestChannel *tmp_chan;
+    fd_set read_fds;
+
+    for (int i = 0; i < params->num_request_channels; ++i) {
+        params->ctl_chan->cwrite("newthread");
+        reply = params->ctl_chan->cread();
+        requestChannels[i] = new RequestChannel(reply, RequestChannel::CLIENT_SIDE);
+
+        cout << "Creating newthread " << '\n';
+        cout << "Response to newthread: "<< " --- " << reply << '\n';
+
+        requestChannels[i]->cwrite("hello");
+        requests[i] = "null";
     }
+
+    while (run) {
+        FD_ZERO(&read_fds);
+        for (int i = 0; i < params->num_request_channels; ++i) {
+            FD_SET(requestChannels[i]->read_fd(), &read_fds);
+        }
+
+        // Wait until a file descriptor is changed
+        select(params->num_request_channels, &read_fds, nullptr, nullptr, nullptr);
+
+        // See what file descriptor is changed
+        for (int i = 0; i < params->num_request_channels; ++i) {
+            // Check if the FD was changed
+            if (FD_ISSET(requestChannels[i]->read_fd(), &read_fds) != 0) {
+                tmp_chan = requestChannels[i];
+                string reply = tmp_chan->cread();
+
+                cout << "Response to : " << requests[i] << " --- " << reply << '\n';
+
+                if (requests[i].find("null") == std::string::npos) {
+                    req_count++;
+                    BoundedBuffer<string> *sbb = lookup(requests[i], params->SSB_container);
+                    sbb->Deposit(reply);
+                }
+
+                string req = params->buff->Remove();
+
+                if (req.find("quit") != string::npos) {
+                    cout << "Ending the Select Watcher" << endl;
+                    quit_count++;
+
+                    if (quit_count == 3)
+                        run = 0;
+                } else {
+                    string reply = tmp_chan->send_request(req);
+                    tmp_chan->cwrite(req);
+                    requests[i] = req;
+                }
+            }
+        }
+    }
+
+    // Clean up
+    cout << "Cleaning up request channels" << endl;
+
+    for (int i = 0; i < params->num_request_channels; ++i) {
+        cout << "Quiting"<< endl;
+        requestChannels[i]->cwrite("quit");
+        reply = requestChannels[i]->cread();
+        cout << reply << endl;
+        delete requestChannels[i];
+    }
+
+    delete[] requestChannels;
     return 0;
 }
 
