dce68e76c9c54d16dc8d58144adf04c741db4615
Joseph Martinsen <joseph@martinsen.com>
2017-11-25 21:48:56 -0600

    Cleaned up mp1

--- mp1/my_allocator.c
+++ mp1/my_allocator.c
@@ -1,7 +1,7 @@
-/* 
+/*
     File: my_allocator.c
 
-    Author: <your name>
+    Author: Joseph Martinsen
             Department of Computer Science
             Texas A&M University
     Date  : <date>
@@ -16,51 +16,138 @@
 /* DEFINES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* INCLUDES */
 /*--------------------------------------------------------------------------*/
 
 #include<stdlib.h>
+#include <math.h>
 #include "my_allocator.h"
+#include "free_list.h"
+#include "utils.h"
+
+// TODO: remove below
+#include<stdio.h>
+
 
 /*--------------------------------------------------------------------------*/
-/* DATA STRUCTURES */ 
+/* DATA STRUCTURES */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
+/* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
 /* CONSTANTS */
 /*--------------------------------------------------------------------------*/
+// Points to head of the linked list
+static FL_HEADER *FREE_LIST_HEAD;
+static FL_HEADER **FREE_LIST_ARRAY;
 
-    /* -- (none) -- */
+// Stored constant of the header size
+#define FL_HEADER_SIZE sizeof(FREE_LIST_HEAD)
 
 /*--------------------------------------------------------------------------*/
 /* FORWARDS */
 /*--------------------------------------------------------------------------*/
 
-    /* -- (none) -- */
-
+/* -- (none) -- */
+// log2(length) - log2(blocksize) + 1
 /*--------------------------------------------------------------------------*/
 /* FUNCTIONS FOR MODULE MY_ALLOCATOR */
 /*--------------------------------------------------------------------------*/
 
-/* Don't forget to implement "init_allocator" and "release_allocator"! */
+unsigned int init_allocator(unsigned int _basic_block_size,
+                            unsigned int _length) {
+    // Create Array of FL_HEADERS
+    int n = (int) determine_array_size(_basic_block_size, _length);
+    FL_HEADER* temp[n];
+    FREE_LIST_ARRAY = temp;
+    long binary_of_length = convert_to_binary(_length);
 
+    // Create blocks accordinaly
+    int power_2_bbs = (int) _log2(_basic_block_size);
+    long binary_check = binary_of_length;
+
+    // Get rid of undeeded binary bits for constructing
+    int iter = 0;
+    while (iter < power_2_bbs) {
+        binary_check /= 10;
+        iter++;
+    }
+
+    printf("\nbinary_check: %ld\n\n", binary_check);
+    int digit_count = (int) get_digit_count(binary_check);
+
+    for (int i = 0; i < digit_count; i++) {
+        // Create the header
+        size_t _len = (size_t) pow(2, power_2_bbs + i);
+        FL_HEADER *current_block = malloc(sizeof(FL_HEADER));
+        current_block->length = _len;
+        current_block->head = NULL;
+        current_block->tail = NULL;
+
+        if (binary_check % 10) {
+            // create a block
+            printf("Create Block: %d\n", (int) _len);
+            FL_HEADER *additional_block = malloc(_len + sizeof(FL_HEADER));
+            additional_block->length = _len;
+            additional_block->head = current_block;
+            additional_block->tail = NULL;
+            current_block->tail = additional_block;
+        } else {
+            // Create only a header
+            printf("Index: %d\n", (int) pow(2, power_2_bbs + i));
+        }
+        FREE_LIST_ARRAY[i] = current_block;
+        binary_check /= 10;
+    }
+
+    // WHY DOES THIS WORK
+    long long int sum = 0;
+    for(int i = 0; i < 12; i++){
+        printf("header_test_len: %zu", FREE_LIST_ARRAY[i]->length);
+        if(FREE_LIST_ARRAY[i]->tail) {
+            sum += FREE_LIST_ARRAY[i]->length;
+            printf("\t EXTRA BLOCK CREATED");
+        }
+        printf("\n");
+    }
+    printf("Sum of Blocks Created: %llu\n\n", sum);
+    return 0;
+}
 
 Addr my_malloc(size_t _length) {
-  /* This preliminary implementation simply hands the call over the 
-     the C standard library! 
-     Of course this needs to be replaced by your implementation.
-  */
-  return malloc(_length);
+    // Find a block of appropriate size
+
+    // WHY DOES THIS NOT WORK - i gets optimized out............. :(
+    long long int sum = 0;
+    for(int i = 0; i < 12; i++){
+        FL_HEADER *temp = FREE_LIST_ARRAY[i];
+        printf("header_test_len: %zu", temp->length);
+        if(FREE_LIST_ARRAY[i]->tail) {
+            sum += FREE_LIST_ARRAY[i]->length;
+            printf("\t EXTRA BLOCK CREATED");
+        }
+        printf("\n");
+    }
+    printf("Sum of Blocks Created: %llu\n\n", sum);
+
+    int index = get_index(_length, FREE_LIST_ARRAY);
+    FL_HEADER *free_list_row = FREE_LIST_ARRAY[index];
+    if (free_list_row->length > _length) {
+        // if row needs to be split
+        return (Addr) FL_split(&free_list_row, free_list_row->tail, _length) - sizeof(FL_HEADER);
+    } else {
+        // Size is just what the doctor ordered
+        return (Addr) FL_remove(&free_list_row, free_list_row->tail) - sizeof(FL_HEADER);
+    }
 }
 
 int my_free(Addr _a) {
-  /* Same here! */
-  free(_a);
-  return 0;
+    FL_HEADER *block = (FL_HEADER *) ((char *) _a + FL_HEADER_SIZE);
+    FL_add(&FREE_LIST_ARRAY, block);
+    return 0;
 }
 
--- mp1/my_allocator.h
+++ mp1/my_allocator.h
@@ -26,13 +26,13 @@
 /* -- (none) -- */
 
 /*--------------------------------------------------------------------------*/
-/* DATA STRUCTURES */ 
+/* DATA STRUCTURES */
 /*--------------------------------------------------------------------------*/
 
-typedef void * Addr; 
+typedef void *Addr;
 
 /*--------------------------------------------------------------------------*/
-/* FORWARDS */ 
+/* FORWARDS */
 /*--------------------------------------------------------------------------*/
 
 /* -- (none) -- */
@@ -41,21 +41,24 @@ typedef void * Addr;
 /* MODULE   MY_ALLOCATOR */
 /*--------------------------------------------------------------------------*/
 
-unsigned int init_allocator(unsigned int _basic_block_size, 
-			    unsigned int _length); 
+unsigned int init_allocator(unsigned int _basic_block_size,
+                            unsigned int _length);
+
 /* This function initializes the memory allocator and makes a portion of 
    ’_length’ bytes available. The allocator uses a ’_basic_block_size’ as 
    its minimal unit of allocation. The function returns the amount of 
    memory made available to the allocator. If an error occurred, 
    it returns 0. 
-*/ 
+*/
 
-int release_allocator(); 
+int release_allocator();
+
 /* This function returns any allocated memory to the operating system. 
    After this function is called, any allocation fails.
-*/ 
+*/
 
-Addr my_malloc(size_t _length); 
+Addr my_malloc(size_t _length);
+
 /* Allocate _length number of bytes of free memory and returns the 
    address of the allocated portion. Returns 0 when out of memory. 
    NOTE: In the old days one used 'unsigned int' for the length. The C
@@ -63,10 +66,10 @@ Addr my_malloc(size_t _length);
    represent memory sizes. This makes the code more portable. Depending
    on the architecture, 'size_t' may or may not be the same 
    as 'unsigned int'. 
-*/ 
+*/
 
-int my_free(Addr _a); 
+int my_free(Addr _a);
 /* Frees the section of physical memory previously allocated 
-   using ’my_malloc’. Returns 0 if everything ok. */ 
+   using ’my_malloc’. Returns 0 if everything ok. */
 
 #endif 
